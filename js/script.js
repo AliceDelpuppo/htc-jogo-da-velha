284e383fb93e3a038155e1fdeaa64efadc8bca15
AliceDelpuppo <alicedelpuppo1@gmail.com>
2021-08-10 18:59:06 -0300

    Correção botão que ativa bot

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const $switchBot = document.querySelector('.switch-bot')
+const $switchPlayer2 = document.querySelector('.switch-player-2')
+const $botName = document.querySelector('.bot-name')
 
 $switchBot.addEventListener('click', function(){
-    $switchBot.classList.toggle('active-bot')
+    $switchPlayer2.classList.toggle('active-bot')
+    $botName.classList.toggle('active-bot')
 })
