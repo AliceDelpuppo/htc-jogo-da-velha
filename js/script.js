8bd8572e9949d96055ef531f3aa1ff7a9c9314f6
AliceDelpuppo <alicedelpuppo1@gmail.com>
2021-08-11 21:48:07 -0300

    Correção espaçamento container left

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const $switchBot = document.querySelector('.switch-bot')
 const $switchPlayer2 = document.querySelector('.switch-player-2')
 const $botName = document.querySelector('.bot-name')
+const $battlefield = document.querySelectorAll('.field')
 
 $switchBot.addEventListener('click', function(){
     $switchPlayer2.classList.toggle('active-bot')
     $botName.classList.toggle('active-bot')
 })
+
+console.log($bot)
